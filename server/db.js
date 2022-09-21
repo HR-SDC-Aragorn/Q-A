8c51831ee0b9d3c6ea9017c55c4463911b14ee67
Stephen Ho <wickedcz4a@gmail.com>
2022-09-21 15:27:33 -0700

    changed db credentials

--- server/db.js
+++ server/db.js
@@ -1,16 +1,25 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: process.env.USER,
+//   database: 'qa',
+//   password: '',
+//   port: 5432,
+//   host: 'localhost'
+// });
+
 const pool = new Pool({
-  user: process.env.USER,
-  database: 'qa',
-  password: '',
-  port: 5432,
-  host: 'localhost'
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
 
 // pool.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res)
 //   pool.end()
 // });
+
 
 module.exports = pool;
